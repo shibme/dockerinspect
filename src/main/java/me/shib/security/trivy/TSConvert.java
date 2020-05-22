d579bb3e49e44d9e11c5035f16b50ca43fbe8899
Shibly Meeran <git@shib.me>
2020-05-22 14:55:06 +0530

    adding a null check

--- src/main/java/me/shib/security/trivy/TSConvert.java
+++ src/main/java/me/shib/security/trivy/TSConvert.java
@@ -22,43 +22,45 @@ class TSConvert {
         StewardData data = new StewardData(projectName, toolName);
         data.addContext(report.getType());
         data.addContext(projectName);
-        for (Vulnerability vulnerability : report.getVulnerabilities()) {
-            String title = "Vulnerability [" + vulnerability.getVulnerabilityId() + "] found in " +
-                    vulnerability.getPackageName() + " of " + report.getTarget();
-            StewardFinding finding = new StewardFinding(title,
-                    vulnerability.getSeverity().getPriority());
-            StringBuilder description = new StringBuilder();
-            description.append("A vulnerable component (**").append(vulnerability.getPackageName())
-                    .append("-").append(vulnerability.getInstalledVersion()).append("**) was found in ")
-                    .append("**").append(report.getTarget()).append("**.\n\n");
-            try {
-                description.append("**[").append(vulnerability.getVulnerabilityId()).append("](")
-                        .append(getUrlForCVE(vulnerability.getVulnerabilityId())).append("):**");
-            } catch (TrivyException e) {
-                description.append("**").append(vulnerability.getVulnerabilityId()).append(":**");
-            }
-            description.append("\n");
-            description.append(" * **Package Name:** ").append(vulnerability.getPackageName()).append("\n");
-            description.append(" * **Installed Version:** ").append(vulnerability.getInstalledVersion()).append("\n");
-            description.append(" * **Fix Version:** ").append(vulnerability.getFixedVersion()).append("\n");
-            description.append(" * **Summary:** ").append(vulnerability.getTitle()).append("\n");
-            description.append(" * **Info:** ").append(vulnerability.getDescription()).append("\n");
-            if (vulnerability.getReferences() != null && vulnerability.getReferences().size() > 0) {
-                description.append("\n\n**References:**\n");
-                for (String reference : vulnerability.getReferences()) {
-                    if (reference.toLowerCase().startsWith("http")) {
-                        description.append(" * [").append(reference).append("](").append(reference).append(")");
-                    } else {
-                        description.append(" * ").append(reference);
-                    }
-                    description.append("\n");
+        if (report.getVulnerabilities() != null) {
+            for (Vulnerability vulnerability : report.getVulnerabilities()) {
+                String title = "Vulnerability [" + vulnerability.getVulnerabilityId() + "] found in " +
+                        vulnerability.getPackageName() + " of " + report.getTarget();
+                StewardFinding finding = new StewardFinding(title,
+                        vulnerability.getSeverity().getPriority());
+                StringBuilder description = new StringBuilder();
+                description.append("A vulnerable component (**").append(vulnerability.getPackageName())
+                        .append("-").append(vulnerability.getInstalledVersion()).append("**) was found in ")
+                        .append("**").append(report.getTarget()).append("**.\n\n");
+                try {
+                    description.append("**[").append(vulnerability.getVulnerabilityId()).append("](")
+                            .append(getUrlForCVE(vulnerability.getVulnerabilityId())).append("):**");
+                } catch (TrivyException e) {
+                    description.append("**").append(vulnerability.getVulnerabilityId()).append(":**");
                 }
+                description.append("\n");
+                description.append(" * **Package Name:** ").append(vulnerability.getPackageName()).append("\n");
+                description.append(" * **Installed Version:** ").append(vulnerability.getInstalledVersion()).append("\n");
+                description.append(" * **Fix Version:** ").append(vulnerability.getFixedVersion()).append("\n");
+                description.append(" * **Summary:** ").append(vulnerability.getTitle()).append("\n");
+                description.append(" * **Info:** ").append(vulnerability.getDescription()).append("\n");
+                if (vulnerability.getReferences() != null && vulnerability.getReferences().size() > 0) {
+                    description.append("\n\n**References:**\n");
+                    for (String reference : vulnerability.getReferences()) {
+                        if (reference.toLowerCase().startsWith("http")) {
+                            description.append(" * [").append(reference).append("](").append(reference).append(")");
+                        } else {
+                            description.append(" * ").append(reference);
+                        }
+                        description.append("\n");
+                    }
+                }
+                finding.setDescription(description.toString());
+                finding.addContext(vulnerability.getVulnerabilityId());
+                finding.addContext(vulnerability.getPackageName());
+                finding.addContext(vulnerability.getSeveritySource());
+                data.addFinding(finding);
             }
-            finding.setDescription(description.toString());
-            finding.addContext(vulnerability.getVulnerabilityId());
-            finding.addContext(vulnerability.getPackageName());
-            finding.addContext(vulnerability.getSeveritySource());
-            data.addFinding(finding);
         }
         return data;
     }
